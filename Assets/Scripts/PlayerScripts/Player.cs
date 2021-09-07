c9b07a6cc7bc85ff59c1fe724b237f0819fd610f
MaruYoon <87841786+MaruYoon@users.noreply.github.com>
2021-09-07 13:47:17 +0900

    up

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -1,22 +1,35 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+
+[RequireComponent(typeof(Rigidbody))]
 
 public class Player : MonoBehaviour
 {
 
 	private Animator anim;
 	private CharacterController controller;
+	private Rigidbody Rigid;
 
+	public float JumpPower = 10.0f;
 
 	public float speed = 0.5f;
 	public float turnSpeed = 0.5f;
 	private Vector3 moveDirection = Vector3.zero;
 	public float gravity = 20.0f;
 
+	private bool IsJumping;
+
 	void Start()
 	{
 		controller = GetComponent<CharacterController>();
 		anim = gameObject.GetComponentInChildren<Animator>();
+
+		Rigid = GetComponent<Rigidbody>();
+		Rigid.useGravity = false;
+
+		IsJumping = false;
 	}
 
 	void Update()
@@ -30,14 +43,6 @@ public class Player : MonoBehaviour
 			anim.SetInteger("AnimationPar", 0);
 		}
 
-		/*
-		if(controller.isGrounded)
-		{
-		}
-
-		moveDirection = transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed;
-		 */
-
 
 		float Hor = Input.GetAxisRaw("Horizontal");
 		float Ver = Input.GetAxisRaw("Vertical");
@@ -50,5 +55,41 @@ public class Player : MonoBehaviour
 		transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
 		controller.Move(moveDirection * Time.deltaTime);
 		moveDirection.y -= gravity * Time.deltaTime;
+
+		Jump();
+
 	}
+
+	void Jump()
+    {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+			if(!IsJumping)
+            {
+				IsJumping = true;
+				Rigid.AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+
+					Rigid.useGravity = true;
+			
+			}
+			else
+			{
+				return;
+			}
+
+			Rigid.useGravity = false;
+		}
+
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            IsJumping = false;
+			Debug.Log("Ãæµ¹");
+		}
+    }
+
+
 }
